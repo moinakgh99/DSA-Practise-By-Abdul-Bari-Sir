72d36fe0fddcc9569c63b7810a073e866b214147
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-06-30 16:43:16 +0530

    Types of recursion

---
+++ Recursion/03_TreeRecursion.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fun1(int n) {
+    if(n > 0) {
+        cout << " " << n;
+        fun1(n-1);
+        fun1(n-1);
+    }
+}
+
+int main() {
+    int x = 3;
+    fun1(x);
+
+    return 0;
+}
---
+++ Recursion/04_IndirectRecursion.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void funA(int n);
+
+void funB(int n) {
+    if(n > 0) {
+        cout << " " << n;
+        funA(n/2);
+    }
+}
+
+void funA(int n) {
+    if(n > 0) {
+        cout << " " << n;
+        funB(n-1);
+    }
+}
+
+int main() {
+    int x = 20;
+    funA(x);
+
+    return 0;
+}
